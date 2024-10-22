00aa09a691d82cbba7844b74eca2c14f74ce2e21
galih <samudragalih94@gmail.com>
2024-10-22 13:11:32 +0700

    feat(contentShape): use contentShape(.rect) on the VStack then the whole area for the stack becomes tappable, including the spacer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,20 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped")
-                }
-            Circle()
-                .fill(.red)
-                .frame(width: 300, height: 300)
-                .contentShape(.rect)
-                .onTapGesture {
-                    print("Circle tapped")
-                }
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("world")
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            print("VStack tapped")
         }
     }
 }
