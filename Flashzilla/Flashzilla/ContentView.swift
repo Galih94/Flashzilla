c9672be50b7a4827fe12e1263ee0ed1fb9d75662
galih <samudragalih94@gmail.com>
2024-10-22 11:31:59 +0700

    feat(Gesture):  rotating views using `RotateGesture`

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount = 0.0
-    @State private var finalAmount = 1.0
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
     
     var body: some View {
         Text("Text")
-            .scaleEffect(finalAmount + currentAmount)
-            .gesture(MagnifyGesture()
+            .rotationEffect(currentAmount + finalAmount)
+            .gesture(RotateGesture()
                 .onChanged{ value in
-                    currentAmount = value.magnification - 1
+                    currentAmount = value.rotation
                 }
                 .onEnded{ value in
                     finalAmount += currentAmount
-                    currentAmount = 0
+                    currentAmount = .zero
                 })
     }
 }
