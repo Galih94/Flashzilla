3dbe24126b082869d593cec33fa87a0c65edda29
galih <samudragalih94@gmail.com>
2024-10-22 13:00:07 +0700

    feat(allowsHitTesting): implement `allowsHitTesting` to make object see through testable object, means object behind it that will be detected

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello world")
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Rectangle tapped")
+                }
+            Circle()
+                .fill(.red)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("Circle tapped")
+                }
+                .allowsHitTesting(false)
+        }
     }
 }
 
