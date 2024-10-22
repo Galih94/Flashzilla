c9a9d83e88ba5ba0e5af2a75fe31bc82346a7b95
galih <samudragalih94@gmail.com>
2024-10-22 11:16:36 +0700

    feat(Gesture): example of tap gesture, long gesture, long gesture with duration and long gesture with detect in progress

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -13,7 +13,24 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("double")
+                .onTapGesture(count: 2, perform: {
+                    print("double tapped")
+                })
+            Text("long")
+                .onLongPressGesture(perform: {
+                    print("long tapped")
+                })
+            Text("long duration 2")
+                .onLongPressGesture(minimumDuration: 2, perform: {
+                    print("long tapped 2 second")
+                })
+            Text("detect pressing changed")
+                .onLongPressGesture(minimumDuration: 2, perform: {
+                    print("long tapped for 2 second")
+                }, onPressingChanged: { inProgress in
+                    print("long tapped in progress \(inProgress)")
+                })
         }
         .padding()
     }
