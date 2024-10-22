51bb269e27abcfe29b83d1b61182934c3b5709eb
galih <samudragalih94@gmail.com>
2024-10-22 12:52:47 +0700

    clean code

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,37 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    /// how fat the sircle has been dragged
-    @State private var offset = CGSize.zero
-    
-    /// wether it is currently being dragged or not
-    @State private var isDragging = false
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-                         
+        Text("Hello world")
     }
 }
 
