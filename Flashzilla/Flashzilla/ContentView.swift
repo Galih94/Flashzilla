1dd12d924c540bf444e1d6da13451d0263740951
galih <samudragalih94@gmail.com>
2024-10-23 09:16:04 +0700

    clean code

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -9,18 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("world")
-        }
-        .contentShape(.rect)
-        .onTapGesture {
-            print("VStack tapped")
-        }
+        Text("Hello world")
     }
 }
 
 #Preview {
     ContentView()
 }
+import SwiftUI
