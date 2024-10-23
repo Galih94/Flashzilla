572fff1233e590f0f1153cfe5f42f60faa88ab68
galih <samudragalih94@gmail.com>
2024-10-23 10:51:26 +0700

    feat(accessibilityReduceTransparency): use `accessibilityReduceTransparency` to get setting user asks to make color more solid

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,22 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var scale = 1.0
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     var body: some View {
-        Button("Hello") {
-            withOptionalAnimation {
-                scale *= 1.5
-            }
-        }
-        .scaleEffect(scale)
-    }
-    
-    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-        if UIAccessibility.isReduceMotionEnabled {
-            return try body()
-        } else {
-            return try withAnimation(animation, body)
-        }
+        Text("Hello")
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
     }
 }
 
