dda89a9129d68045ab6479ba3d90d01a2cbb079b
galih <samudragalih94@gmail.com>
2024-10-23 10:03:22 +0700

    clean code

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,22 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 3, tolerance: 1, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
     var body: some View {
         Text("Hello world")
-            .onReceive(timer, perform: { time in
-                if counter > 4 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("time is: \(time)")
-                }
-                counter += 1
-            })
     }
 }
 
 #Preview {
     ContentView()
 }
-import SwiftUI
