0774c4342bc4020996db9bb99536d7dfc2195fa7
galih <samudragalih94@gmail.com>
2024-10-23 09:27:15 +0700

    feat(Timer): use `Timer.publish` and detect publish in `onReceive()` for 5 second and launch every 1 second

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
     var body: some View {
         Text("Hello world")
+            .onReceive(timer, perform: { time in
+                if counter > 4 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("time is: \(time)")
+                }
+                counter += 1
+            })
     }
 }
 
