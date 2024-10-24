fa474baf160411f8466007909d48409f1082a6ce
galih <samudragalih94@gmail.com>
2024-10-24 10:57:12 +0700

    feat(Remove Drag): add drag gesture that detect offset drag ui and remove on drag gesture in content view

--- Flashzilla/Flashzilla/View/CardView.swift
+++ Flashzilla/Flashzilla/View/CardView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
+    @State private var offset = CGSize.zero
     @State private var isShowingAnswer = false
     var body: some View {
         ZStack {
@@ -30,6 +32,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(offset.width / 5.0))
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(DragGesture()
+            .onChanged { gesture in
+                offset = gesture.translation
+            }
+            .onEnded { _ in
+                if abs(offset.width) > 100 {
+                    /// remove card
+                    removal?()
+                } else {
+                    offset = .zero
+                }
+            }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
