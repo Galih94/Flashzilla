6a48147a0850c33e528d03fd095ed000b85881f3
galih <samudragalih94@gmail.com>
2024-10-25 09:55:47 +0700

    feat(Timer): implement timer

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var diffWithoutColor
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -17,6 +19,13 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -49,6 +58,13 @@ struct ContentView: View {
                 }
             }
         }
+        .onReceive(timer, perform: { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                timer.upstream.connect().cancel()
+            }
+        })
     }
     
     private func removeCard(at index: Int) {
