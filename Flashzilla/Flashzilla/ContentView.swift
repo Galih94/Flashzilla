c510375083d5a8e7f767faa9a75a105eb20684a2
galih <samudragalih94@gmail.com>
2024-10-28 09:00:44 +0700

    fix(CardView): fix to allow slide only on top card

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
